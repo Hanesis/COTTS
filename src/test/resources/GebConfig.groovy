32e563abb903180b980361186afb1ff15839a28f
Kalous <j.kalous@cz.gmc.net>
2019-09-14 09:21:11 +0200

    added more waiting options for geb tests

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -1,44 +1,51 @@
-/*
-	This is the Geb configuration file.
-
-	See: http://www.gebish.org/manual/current/#configuration
-*/
-
-
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
 
 waiting {
-	timeout = 2
+    includeCauseInMessage = true
+    timeout = 15
+    retryInterval = 0.5
+    presets {
+        pages {
+            timeout = 1
+        }
+        quick {
+            timeout = 2
+        }
+        medium {
+            timeout = 5
+        }
+        WFListRefreshPeriod {
+            timeout = 5
+        }
+    }
 }
 
 environments {
 
-	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
-	chrome {
-		driver = { new ChromeDriver() }
-	}
+    // See: http://code.google.com/p/selenium/wiki/ChromeDriver
+    chrome {
+        driver = { new ChromeDriver() }
+    }
 
-	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
-	chromeHeadless {
-		driver = {
-			ChromeOptions o = new ChromeOptions()
-			o.addArguments('headless')
-			new ChromeDriver(o)
-		}
-	}
+    // See: http://code.google.com/p/selenium/wiki/ChromeDriver
+    chromeHeadless {
+        driver = {
+            ChromeOptions o = new ChromeOptions()
+            o.addArguments('headless')
+            new ChromeDriver(o)
+        }
+    }
 
-	// See: http://code.google.com/p/selenium/wiki/FirefoxDriver
-	firefox {
-		atCheckWaiting = 1
+    // See: http://code.google.com/p/selenium/wiki/FirefoxDriver
+    firefox {
+        atCheckWaiting = 1
 
-		driver = { new FirefoxDriver() }
-	}
+        driver = { new FirefoxDriver() }
+    }
 
 }
-
-// To run the tests with all browsers just run “./gradlew test”
 
 baseUrl = "http://gebish.org" // TODO: this needs to be changed when actually testing
 requirePageAtCheckers = true
