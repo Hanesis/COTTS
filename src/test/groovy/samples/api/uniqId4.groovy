e0d465cf1becf16eb6b051d5c63870b2dd7c2073
venca256 <venda.mares@seznam.cz>
2019-09-14 12:17:17 +0200

    New test

---
+++ src/test/groovy/samples/api/uniqId4.groovy
@@ -0,0 +1,25 @@
+package samples.api
+
+import groovy.json.JsonSlurper
+import org.junit.jupiter.api.Test
+
+import static io.restassured.RestAssured.get
+
+// Must be run via JUNIT not via Gradle Tasks - Theyre made for GEB tests
+class uniqId4 {
+    @Test
+    void getGoogleBody() {
+        given:
+        def response = get('http://157.230.77.139:3000/v1/client').then()
+                .assertThat()
+                .statusCode(200)
+                .extract()
+                .asString()
+        JsonSlurper slurper = new JsonSlurper()
+        def object = slurper.parseText(response)
+        println(object)
+        println("asda")
+        println(object.body._id)
+//        assert result._id == "Guillaume"
+    }
+}
