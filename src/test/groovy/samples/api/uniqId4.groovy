270217c7756e8c137b8dcfa3a5a9862ebcf734d1
venca256 <venda.mares@seznam.cz>
2019-09-14 12:49:26 +0200

    Final test

--- src/test/groovy/samples/api/uniqId4.groovy
+++ src/test/groovy/samples/api/uniqId4.groovy
@@ -1,6 +1,7 @@
 package samples.api
 
 import groovy.json.JsonSlurper
+import org.junit.Assert
 import org.junit.jupiter.api.Test
 
 import static io.restassured.RestAssured.get
@@ -8,18 +9,36 @@ import static io.restassured.RestAssured.get
 // Must be run via JUNIT not via Gradle Tasks - Theyre made for GEB tests
 class uniqId4 {
     @Test
-    void getGoogleBody() {
+    void checkUniqId() {
         given:
-        def response = get('http://157.230.77.139:3000/v1/client').then()
-                .assertThat()
-                .statusCode(200)
-                .extract()
-                .asString()
-        JsonSlurper slurper = new JsonSlurper()
-        def object = slurper.parseText(response)
-        println(object)
-        println("asda")
-        println(object.body._id)
-//        assert result._id == "Guillaume"
+        for (i = 0; i <50; i++) {
+            def id = 0
+            def response = get('http://157.230.77.139:3000/v1/client').then()
+                    .assertThat()
+                    .statusCode(200)
+                    .extract()
+                    .asString()
+            JsonSlurper slurper = new JsonSlurper()
+            def object = slurper.parseText(response)
+            Assert.assertNotEquals(object.body._id, id);
+            println(object.body._id)
+        }
+
+    }
+
+    @Test
+    void checkCalculator() {
+        given:
+        for (def i = 0; i <50; i++) {
+            def id = 0
+            def response = get('http://157.230.77.139:3000/v1/client').then()
+                    .assertThat()
+                    .statusCode(200)
+                    .extract()
+                    .asString()
+            JsonSlurper slurper = new JsonSlurper()
+            def object = slurper.parseText(response)
+            Assert.assertNotEquals(object.body._id, id);
+        }
     }
 }
